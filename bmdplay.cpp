1357bfb01392c0649dd978c98be0c224ae97cd62
Luca Barbato <lu_zero@gentoo.org>
2017-01-31 23:44:51 +0000

    bmdplay: Convert to the decoupled decoding API

--- bmdplay.cpp
+++ bmdplay.cpp
@@ -394,7 +394,8 @@ int main(int argc, char *argv[])
                     exit(1);
                 }
 
-                if (avcodec_open2(avctx, codec, NULL) < 0) {
+                if (avcodec_parameters_to_context(avctx, par) < 0 ||
+                    avcodec_open2(avctx, codec, NULL) < 0) {
                     avcodec_free_context(&avctx);
                     av_log(NULL, AV_LOG_ERROR, "Codec open failed\n");
                     exit(1);
@@ -685,6 +686,8 @@ void Player::StopRunning()
 void Player::ScheduleNextFrame(bool prerolling)
 {
     AVPacket pkt;
+    void *frame;
+    int ret;
 
     if (serial_fd > 0 && packet_queue_get(&dataqueue, &pkt, 0)) {
         if (pkt.data[0] != ' '){
@@ -704,12 +707,13 @@ void Player::ScheduleNextFrame(bool prerolling)
                                        pix,
                                        bmdFrameFlagDefault,
                                        &videoFrame);
-    void *frame;
-    int got_picture;
     videoFrame->GetBytes(&frame);
 
-    avcodec_decode_video2(video.codec, avframe, &got_picture, &pkt);
-    if (got_picture) {
+    avcodec_send_packet(video.codec, &pkt);
+
+    // TODO: support receiving multiple frames
+    ret = avcodec_receive_frame(video.codec, avframe);
+    if (ret >= 0) {
         uint8_t *data[4];
         int linesize[4];
 
